3530f992298004bfd947f442278d9de1829a9833
OverlordJ-J <50602122+OverlordJ-J@users.noreply.github.com>
2020-11-06 23:42:47 -0600

    Petiso avance
    Base del juego con bases del reconocimiento.

---
+++ Bellum_non_deficient/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Bellum_non_deficient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnAtaque = new System.Windows.Forms.Button();
+            this.listBoxReco = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // btnAtaque
+            // 
+            this.btnAtaque.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtaque.Location = new System.Drawing.Point(626, 24);
+            this.btnAtaque.Name = "btnAtaque";
+            this.btnAtaque.Size = new System.Drawing.Size(107, 45);
+            this.btnAtaque.TabIndex = 1;
+            this.btnAtaque.Text = "Ataque";
+            this.btnAtaque.UseVisualStyleBackColor = true;
+            this.btnAtaque.Click += new System.EventHandler(this.btnAtaque_Click);
+            // 
+            // listBoxReco
+            // 
+            this.listBoxReco.FormattingEnabled = true;
+            this.listBoxReco.Location = new System.Drawing.Point(626, 100);
+            this.listBoxReco.Name = "listBoxReco";
+            this.listBoxReco.Size = new System.Drawing.Size(125, 251);
+            this.listBoxReco.TabIndex = 2;
+            this.listBoxReco.SelectedIndexChanged += new System.EventHandler(this.listBoxReco_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 524);
+            this.Controls.Add(this.listBoxReco);
+            this.Controls.Add(this.btnAtaque);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnAtaque;
+        private System.Windows.Forms.ListBox listBoxReco;
+    }
+}
+
---
+++ Bellum_non_deficient/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Globalization;
+
+namespace Bellum_non_deficient
+{
+    public partial class Form1 : Form
+    {
+        CultureInfo Cultura = new CultureInfo("es-ES");
+        SpeechRecognitionEngine Reconocedor;
+        Choices Palabras;
+        Grammar Gramatica;
+        int[,] Casillas = new int[5, 5];
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i=0;i<5;i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Casillas[i , j] = 0;
+                }
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(Pens.White, 100, 0, 100, 500);
+            e.Graphics.DrawLine(Pens.White, 200, 0, 200, 500);
+            e.Graphics.DrawLine(Pens.White, 300, 0, 300, 500);
+            e.Graphics.DrawLine(Pens.White, 400, 0, 400, 500);
+            e.Graphics.DrawLine(Pens.White, 0, 100, 500, 100);
+            e.Graphics.DrawLine(Pens.White, 0, 200, 500, 200);
+            e.Graphics.DrawLine(Pens.White, 0, 300, 500, 300);
+            e.Graphics.DrawLine(Pens.White, 0, 400, 500, 400);
+
+            if (Casillas[0, 0] == 1)
+            {
+                Equis(e.Graphics, 0, 0);
+            }
+            if (Casillas[0, 0] == 2)
+            {
+                Cero(e.Graphics, 0, 0);
+            }
+
+            if (Casillas[0, 1] == 1)
+            {
+                Equis(e.Graphics, 200, 0);
+            }
+            if (Casillas[0, 1] == 2)
+            {
+                Cero(e.Graphics, 200, 0);
+            }
+        }
+        void Equis(Graphics G, int x, int y)
+        {
+            Pen Pluma = new Pen(Color.YellowGreen, 5.0f);
+            G.DrawLine(Pluma, x + 20, y + 20, x + 180, y + 180);
+            G.DrawLine(Pluma, x + 20, y + 180, x + 180, y + 20);
+        }
+
+        void Cero(Graphics G, int x, int y)
+        {
+            Pen Pluma = new Pen(Color.YellowGreen, 5.0f);
+            G.DrawEllipse(Pluma, x + 20, y + 20, 160, 160);
+        }
+
+        private void btnAtaque_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Palabras = new Choices(new string[] { "Uno uno", "uno dos" });
+                GrammarBuilder ConstructorGramatica = new GrammarBuilder(Palabras);
+                ConstructorGramatica.Culture = Cultura;
+                Gramatica = new Grammar(ConstructorGramatica);
+
+                Reconocedor = new SpeechRecognitionEngine(Cultura);
+                //Reconocedor.LoadGrammar(Gramatica);
+                Reconocedor.LoadGrammar(new DictationGrammar());
+                Reconocedor.SetInputToDefaultAudioDevice();
+
+                Reconocedor.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(ReconocimientoVoz);
+                Reconocedor.RecognizeAsync(RecognizeMode.Multiple);
+                listBoxReco.Items.Add("Iniciando...");
+                listBoxReco.Update();
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+
+        private void ReconocimientoVoz(object sender, SpeechRecognizedEventArgs e)
+        {
+            listBoxReco.Items.Clear();
+            foreach (var elemento in e.Result.Alternates)
+            {
+                listBoxReco.Items.Add(elemento.Text + ", Confidencia: " + elemento.Confidence.ToString());
+                if (elemento.Text == "Uno uno equis")
+                {
+                    if (Casillas[0, 0] == 0)
+                        Casillas[0, 0] = 1;
+                    else
+                        MessageBox.Show("Intenta en otra casilla");
+                    break;
+                }
+                if (elemento.Text == "uno uno cero")
+                {
+                    if (Casillas[0, 0] == 0)
+                        Casillas[0, 0] = 2;
+                    else
+                        MessageBox.Show("Intenta en otra casilla");
+                    break;
+                }
+                if (elemento.Text == "uno dos equis")
+                {
+                    if (Casillas[0, 1] == 0)
+                        Casillas[0, 1] = 1;
+                    else
+                        MessageBox.Show("Intenta en otra casilla");
+                    break;
+                }
+                if (elemento.Text == "uno dos cero")
+                {
+                    if (Casillas[0, 1] == 0)
+                        Casillas[0, 1] = 2;
+                    else
+                        MessageBox.Show("Intenta en otra casilla");
+                    break;
+                }
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void listBoxReco_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
